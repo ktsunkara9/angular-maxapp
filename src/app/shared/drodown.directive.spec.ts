57fc4e556f63096a29385c1f48a9cc61d50a0904
Sunkara Krishna Teja <62112826+ktsunkara9@users.noreply.github.com>
2021-06-16 00:23:30 +0530

    Recipes (#6)
    * Committin Changes to Shopping List Component
    
    * Added Dropdowns

---
+++ src/app/shared/drodown.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DrodownDirective } from './drodown.directive';
+
+describe('DrodownDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DrodownDirective();
+    expect(directive).toBeTruthy();
+  });
+});
